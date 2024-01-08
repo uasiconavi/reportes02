f1d36af7ce8149e19b0e5159dc169a307759945f
uasiconavi <uasi.conavi@gmail.com>
2024-01-08 06:25:02 -0600

    Mejorando apariencia página fotos

--- lib/pages/fotos_page.dart
+++ lib/pages/fotos_page.dart
@@ -15,93 +15,67 @@ class _FotosPageState extends State<FotosPage> {
   @override
   Widget build(BuildContext context) {
     int cantFotos = context.watch<ReportProvider>().fotos.length;
-    return ListView(
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
-        Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 35.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    'Cantidad de fotos: ',
-                    style: TextStyle(
-                      fontSize: 17.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black87,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                  Text(
-                    '$cantFotos',
-                    style: const TextStyle(
-                      fontSize: 17.0,
-                      fontWeight: FontWeight.normal,
-                      color: Colors.black87,
-                    ),
-                  ),
-                ],
+            const Text(
+              'Cantidad de fotos: ',
+              style: TextStyle(
+                fontSize: 17.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.black87,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              '$cantFotos',
+              style: const TextStyle(
+                fontSize: 17.0,
+                fontWeight: FontWeight.normal,
+                color: Colors.black87,
               ),
             ),
-            cantFotos > 0
-                ? const Padding(
-                    padding: EdgeInsets.only(bottom: 50.0),
-                    child: MostrarFotos(),
-                  )
-                : const SizedBox(
-                    height: 175.0,
-                  ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton.icon(
-                  icon: const Icon(Icons.camera_alt),
-                  label: const Text('Cámara'),
-                  onPressed: cantFotos < 10
-                      ? () async {
-                          tomarFoto().then((foto) {
-                            if (foto != null) {
-                              context.read<ReportProvider>().addFoto(foto);
-                            }
-                          });
-                        }
-                      : _mensajeMax,
-                ),
-              ],
-            ),
-            cantFotos > 1
-                ? Padding(
-                    padding: const EdgeInsets.only(top: 40.0),
-                    child: Column(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: [
-                            ElevatedButton.icon(
-                              icon: const Icon(Icons.restart_alt),
-                              label: const Text('Reiniciar'),
-                              onPressed:
-                                  context.read<ReportProvider>().clearFotos,
-                            ),
-                            const BotonSiguiente(),
-                          ],
-                        ),
-                      ],
-                    ),
-                  )
-                : const Padding(
-                    padding: EdgeInsets.only(top: 50.0),
-                    child: Text(
-                      "Debe tomar entre 2 y 10 fotos para realizar el reporte del daño.",
-                      style: TextStyle(fontSize: 13.0),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
           ],
         ),
+        cantFotos > 0 ? const MostrarFotos() : const SizedBox(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton.icon(
+              icon: const Icon(Icons.camera_alt),
+              label: const Text('Cámara'),
+              onPressed: cantFotos < 10
+                  ? () async {
+                      tomarFoto().then((foto) {
+                        if (foto != null) {
+                          context.read<ReportProvider>().addFoto(foto);
+                        }
+                      });
+                    }
+                  : _mensajeMax,
+            ),
+          ],
+        ),
+        cantFotos > 1
+            ? Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton.icon(
+                    icon: const Icon(Icons.restart_alt),
+                    label: const Text('Reiniciar'),
+                    onPressed: context.read<ReportProvider>().clearFotos,
+                  ),
+                  const BotonSiguiente(),
+                ],
+              )
+            : const Text(
+                "Debe tomar entre 2 y 10 fotos para realizar el reporte del daño.",
+                style: TextStyle(fontSize: 13.0),
+                textAlign: TextAlign.center,
+              ),
       ],
     );
   }
