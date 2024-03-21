5f040b5ae7b7b2b3f0a1987ffa5a6ffc5aa45beb
uasiconavi <uasi.conavi@gmail.com>
2024-03-21 05:58:29 -0600

    Tratando  de hacer el Badge como una elipse

--- lib/components/menu_esquina_derecha.dart
+++ lib/components/menu_esquina_derecha.dart
@@ -25,66 +25,90 @@ class _MenuEsquinaDerechaState extends State<MenuEsquinaDerecha> {
       "Cerrar sesión",
       "Reportes pendientes  (${reportes.length})",
     ];
-    return Badge(
-      showBadge: reportes.isEmpty ? false : true,
-      badgeContent: Text(
-        reportes.length.toString(),
-        style: const TextStyle(
-          fontSize: 11.0,
-          color: Colors.white,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-      position: BadgePosition.topStart(top: 2, start: -21),
-      badgeStyle: const BadgeStyle(badgeColor: Color(0xFF000000)),
-      child: DropdownButtonHideUnderline(
-        child: DropdownButton<String>(
-            value: usuario,
-            style: const TextStyle(
-              color: Colors.black,
-              fontSize: 10.5,
-            ),
-            items: menu.map((String item) {
-              return DropdownMenuItem(
-                value: item,
-                child: Text(item),
-              );
-            }).toList(),
-            onChanged: (String? nuevoValor) {
-              if (nuevoValor == "Cerrar sesión") {
-                showDialog(
-                  context: context,
-                  builder: (context) => AlertDialog(
-                    title: const Text("¿Desea cerrar la sesión?"),
-                    actions: [
-                      TextButton(
-                        onPressed: signOut,
-                        child: const Text(
-                          "Sí",
-                          style: TextStyle(color: Colors.black),
-                        ),
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: const Text(
-                          "No",
-                          style: TextStyle(color: Colors.black),
-                        ),
-                      ),
-                    ],
-                  ),
+    return Stack(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(right: 14.0, top: 5.0),
+          child: DropdownButtonHideUnderline(
+            child: DropdownButton<String>(
+              value: usuario,
+              style: const TextStyle(
+                color: Colors.black,
+                fontSize: 10.5,
+              ),
+              items: menu.map((String item) {
+                return DropdownMenuItem(
+                  value: item,
+                  child: Text(item),
                 );
-              }
-              if (nuevoValor == "Reportes pendientes  (${reportes.length})") {
-                Navigator.push(
+              }).toList(),
+              onChanged: (String? nuevoValor) {
+                if (nuevoValor == "Cerrar sesión") {
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: const Text("¿Desea cerrar la sesión?"),
+                      actions: [
+                        TextButton(
+                          onPressed: signOut,
+                          child: const Text(
+                            "Sí",
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: const Text(
+                            "No",
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                }
+                if (nuevoValor == "Reportes pendientes  (${reportes.length})") {
+                  Navigator.push(
                     context,
                     MaterialPageRoute(
-                        builder: (context) => const ReportesPendientesPage()));
-              }
-            }),
-      ),
+                      builder: (context) => const ReportesPendientesPage(),
+                    ),
+                  );
+                }
+              },
+            ),
+          ),
+        ),
+        Positioned(
+          top: -5,
+          right: -5,
+          child: ClipOval(
+            child: Container(
+              color: const Color(0xFF000000),
+              child: Padding(
+                padding: const EdgeInsets.all(3.0),
+                child: Badge(
+                  showBadge: reportes.isNotEmpty,
+                  badgeContent: Text(
+                    reportes.length.toString(),
+                    style: const TextStyle(
+                      fontSize: 10.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  badgeStyle: const BadgeStyle(
+                    badgeColor: Color(0xFF000000),
+                    shape: BadgeShape.circle,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 
